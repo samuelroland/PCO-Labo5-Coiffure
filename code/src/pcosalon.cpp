ed118a84cd85b2af68d002fd6e3943419e1fdbfa
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-11-29 11:00:41 +0100

    Import labo files

---
+++ code/src/pcosalon.cpp
@@ -0,0 +1,161 @@
+/*  _____   _____ ____    ___   ___ ___  ____
+ * |  __ \ / ____/ __ \  |__ \ / _ \__ \|___ \
+ * | |__) | |   | |  | |    ) | | | | ) | __) |
+ * |  ___/| |   | |  | |   / /| | | |/ / |__ <
+ * | |    | |___| |__| |  / /_| |_| / /_ ___) |
+ * |_|     \_____\____/  |____|\___/____|____/
+ */
+// Modifications à faire dans le fichier
+
+#include "pcosalon.h"
+
+#include <pcosynchro/pcothread.h>
+
+#include <iostream>
+
+PcoSalon::PcoSalon(GraphicSalonInterface *interface, unsigned int capacity)
+    : _interface(interface)
+{
+    // TODO
+}
+
+/********************************************
+ * Méthodes de l'interface pour les clients *
+ *******************************************/
+unsigned int PcoSalon::getNbClient()
+{
+    // TODO
+}
+
+bool PcoSalon::accessSalon(unsigned clientId)
+{
+    // TODO
+}
+
+
+void PcoSalon::goForHairCut(unsigned clientId)
+{
+    // TODO
+}
+
+void PcoSalon::waitingForHairToGrow(unsigned clientId)
+{
+    // TODO
+}
+
+
+void PcoSalon::walkAround(unsigned clientId)
+{
+    // TODO
+}
+
+
+void PcoSalon::goHome(unsigned clientId){
+    // TODO
+}
+
+
+/********************************************
+ * Méthodes de l'interface pour le barbier  *
+ *******************************************/
+void PcoSalon::goToSleep()
+{
+    // TODO
+}
+
+
+void PcoSalon::pickNextClient()
+{
+    // TODO
+}
+
+
+void PcoSalon::waitClientAtChair()
+{
+    // TODO
+}
+
+
+void PcoSalon::beautifyClient()
+{
+    // TODO
+}
+
+/********************************************
+ *    Méthodes générales de l'interface     *
+ *******************************************/
+bool PcoSalon::isInService()
+{
+    // TODO
+}
+
+
+void PcoSalon::endService()
+{
+    // TODO
+}
+
+/********************************************
+ *   Méthodes privées pour les animations   *
+ *******************************************/
+void PcoSalon::animationClientAccessEntrance(unsigned clientId)
+{
+    _mutex.unlock();
+    _interface->clientAccessEntrance(clientId);
+    _mutex.lock();
+}
+
+void PcoSalon::animationClientSitOnChair(unsigned clientId, unsigned clientSitNb)
+{
+    _mutex.unlock();
+    _interface->clientSitOnChair(clientId, clientSitNb);
+    _mutex.lock();
+}
+
+void PcoSalon::animationClientSitOnWorkChair(unsigned clientId)
+{
+    _mutex.unlock();
+    _interface->clientSitOnWorkChair(clientId);
+    _mutex.lock();
+}
+
+void PcoSalon::animationClientWaitForHairToGrow(unsigned clientId)
+{
+    _mutex.unlock();
+    _interface->clientWaitHairToGrow(clientId, true);
+    _mutex.lock();
+}
+
+void PcoSalon::animationClientWalkAround(unsigned clientId)
+{
+    _mutex.unlock();
+    _interface->clientWalkAround(clientId);
+    _mutex.lock();
+}
+
+void PcoSalon::animationBarberGoToSleep()
+{
+    _mutex.unlock();
+    _interface->barberGoToSleep();
+    _mutex.lock();
+}
+
+void PcoSalon::animationWakeUpBarber()
+{
+    _mutex.unlock();
+    _interface->clientWakeUpBarber();
+    _mutex.lock();
+}
+
+void PcoSalon::animationBarberCuttingHair()
+{
+    _mutex.unlock();
+    _interface->barberCuttingHair();
+    _mutex.lock();
+}
+
+void PcoSalon::animationClientGoHome(unsigned clientId){
+    _mutex.unlock();
+    _interface->clientWaitHairToGrow(clientId, false);
+    _mutex.lock();
+}
